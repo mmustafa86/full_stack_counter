01b622047ef925e50f68b5bdf2cde83930c55504
Juan Mrad <juansmrad@gmail.com>
2019-12-14 13:09:54 -0600

    add database

--- index.js
+++ index.js
@@ -1,30 +1,57 @@
 const express = require("express");
 const app = express();
+const promise = require('bluebird');
 
-let counter = 0;
+// pg-promise initialization options:
+const initOptions = {
+  // Use a custom promise library, instead of the default ES6 Promise:
+  promiseLib: promise, 
+};
+
+// Database connection parameters:
+const config = {
+  host: 'localhost',
+  port: 5432,
+  database: 'full_stack_counter',
+  user: 'postgres',
+};
+
+const pgp = require('pg-promise')(initOptions);
+const db = pgp(config);
 
 app.use(express.static(__dirname + '/public'));
 
 
 app.get("/value", function (req, response){
-  response.send(counter.toString());
+  db.one("SELECT SUM(current_count) AS count_sum FROM counter")
+  .then(function (result) {
+    console.log(result);
+    response.send(result.count_sum);
+  });
 });
 
 app.get("/increment", function (req, response){
-  counter++;
-  response.send(counter.toString());
+  db.one(`INSERT INTO counter("current_count") VALUES(1); 
+  SELECT SUM(current_count) AS count_sum FROM counter`)
+  .then(function (result){
+    console.log(result);
+    response.send(result.count_sum);
+  });
 });
 
-app.get("/reset", function (req, response){
-  counter = 0;
-  response.send(counter.toString());
+app.get("/reset", function (req, response) {
+  db.result(`INSERT INTO counter(current_count) 
+  VALUES (0 - (SELECT SUM(current_count) AS count_sum FROM counter))`)
+  .then(function (result){
+    response.json(0);
+  });
 });
 
 
-app.get("/decrease", function (req, response){
-  counter--;
-  response.send(counter.toString());
-});
+// app.get("/decrease", function (req, response){
+//   counter--;
+//   response.send(counter.toString());
+// });
 
 app.listen(3000, function(){
   console.log("server listening on post 3000");
